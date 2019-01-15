326ca204991a70b91572f7352916e539dd0dc913
Alex Popa <alex.popa@gmail.com>
2019-01-15 13:00:17 -0500

    Non postgres branches should use sqlite3

--- sample/sample/settings.py
+++ sample/sample/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'sample.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '##REPLACE_NAME##',
-        'USER': '##REPLACE_USER##',
-        'PASSWORD': '##REPLACE_PASS##',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
